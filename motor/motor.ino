7410c197237fabf41a4c014606c14f4d78921944
Ashish Kannur <ashishkannur91@gmail.com>
2020-05-20 18:34:52 +0100

    first commit

---
+++ motor/motor.ino
@@ -0,0 +1,16 @@
+int motorPin = 27;
+ 
+void setup() {
+ 
+  pinMode(motorPin, OUTPUT);
+ 
+}
+ 
+void loop() {
+ 
+  //digitalWrite(motorPin, HIGH);
+  delay(1000);
+  digitalWrite(motorPin, LOW);
+  delay(1000);
+  
+}
---
+++ water/water.ino
@@ -0,0 +1,15 @@
+const int potPin = 13;
+
+int potValue = 0;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+}
+
+void loop() {
+  // Reading potentiometer value
+  potValue = analogRead(potPin);
+  Serial.println(potValue);
+  delay(500);
+}
